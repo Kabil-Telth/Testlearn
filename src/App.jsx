632ee985acec051c286915ae26466c0c8061dea4
Kabil-Telth <kabilan@mytelth.com>
2026-03-20 15:30:14 +0530

    Add additional console log for debugging

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import ThreeScene from './modal/scenecreation'
 function App() {
   const [count, setCount] = useState(0)
   console.log("pushing commit")
+  console.log("againf or checkin")
+
 
   return (
     <>
